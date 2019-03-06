a885e98aacb6c8a0ce32764232cf52ecccfcdf1a
SIMFOR078 <matthielondeix@gmail.com>
2019-03-06 16:32:31 +0100

    retour arrière

--- Back/src/main/java/usmvolley/JDBCWebSecurity.java
+++ Back/src/main/java/usmvolley/JDBCWebSecurity.java
@@ -3,13 +3,17 @@ package usmvolley;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class JDBCWebSecurity extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
@@ -29,9 +33,10 @@ public class JDBCWebSecurity extends WebSecurityConfigurerAdapter{
         http.authorizeRequests()
     	.antMatchers("/").permitAll()
     	.antMatchers("/public").permitAll()
+    	.antMatchers("/joueurs/get/joueurs").permitAll()
     	.antMatchers("/deny").denyAll()
     	.antMatchers("/manifestations").hasAnyAuthority("Captaine", "Bureau")
-    	.antMatchers("/joueurs").hasAnyAuthority("Bureau")
+    	.antMatchers("/users").hasAnyAuthority("Bureau")
     	.anyRequest().authenticated()
     	.and()
     	.formLogin().permitAll()
